97aa61f1b2a01877ef9b37b5c94098873fff9607
Krzysztof Wojnar <krzysztofwojnar@outlook.com>
2023-06-21 20:18:28 +0200

    login, chat and lobby component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { LobbyPageComponent } from './components/lobby-page/lobby-page.component';
+import { ChatPageComponent } from './components/chat-page/chat-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: LoginPageComponent },
+  { path: 'lobby', component: LobbyPageComponent},
+  { path: 'chat', component: ChatPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
