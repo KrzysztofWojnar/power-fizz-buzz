aa9cd16ac0c7fad75e0ff5a153b0c1e3524ee181
Krzysztof Wojnar <krzysztofwojnar@outlook.com>
2023-10-10 21:00:14 +0200

    logo added, old login form removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-import { LoginFormComponent } from './components/login-page/login-form/login-form.component';
 import { LobbyPageComponent } from './components/lobby-page/lobby-page.component';
 import { ChatPageComponent } from './components/chat-page/chat-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -14,7 +13,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     AppComponent,
     LoginPageComponent,
-    LoginFormComponent,
     LobbyPageComponent,
     ChatPageComponent,
     NavbarComponent
